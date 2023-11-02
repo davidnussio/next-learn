2e36a35e1d305e9fab05bf0cbdae81cec0d8edff
David Nussio <david.nussio@gmail.com>
2023-11-02 13:41:44 +0100

    fix: image auth

--- auth.config.ts
+++ auth.config.ts
@@ -7,14 +7,20 @@ export const authConfig = {
   },
   callbacks: {
     authorized({ auth, request: { nextUrl } }) {
+      console.log("authorized", !!auth?.user, nextUrl);
       const isLoggedIn = !!auth?.user;
       const isOnDashboard = nextUrl.pathname.startsWith("/dashboard");
       if (isOnDashboard) {
-        if (isLoggedIn) return true;
+        if (isLoggedIn) {
+          console.log("<- isOnDashboard && isLoggedIn", nextUrl);
+          return true;
+        }
+        console.log("<- isOnDashboard && !isLoggedIn", nextUrl);
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
         return Response.redirect(new URL("/dashboard", nextUrl));
       }
+      console.log("<- !isOnDashboard && !isLoggedIn", nextUrl);
       return true;
     },
   },
